36bdb0dcb77c62074e04b0e3e171d360a84c9d2a
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-02 17:06:02 +0100

    third milestone

--- js/app.js
+++ js/app.js
@@ -3,18 +3,30 @@ new Vue({
     data: {
 
         // array di oggetti dischi
-        discs: []
+        discs: [],
+
+        // selected impostata 'vuota' per il v-model HTML
+        selected: '',
+
+        // array dei generi musicali generato dinamicamente
+        genres: []
     },
     mounted(){
 
         // creo una costante con valore 'this'
         // per poter richiamare i data nel mounted
         const self=this;
-        axios.get('Access-Control-Allow-Origin:http://localhost:8888/php-ajax-dischi/app/server.php')
+        axios.get('http://localhost:8888/php-ajax-dischi/app/server.php')
         .then(function(resp){
             self.discs=resp.data;
+            console.log(self.discs);
+
+            self.discs.forEach((element)=>{
+                if(!self.genres.includes(element.genre)){
+                    self.genres.push(element.genre)
+                }
+            })
         });
-        console.log(self.discs)
     }
 });
 Vue.config.devtools=true;
